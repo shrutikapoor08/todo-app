71f07c51ab038fc9d7371474836ae8f3f58ba86c
Shruti Kapoor <shrkapoor@paypal.com>
2019-10-04 14:34:16 -0700

    first commit

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React, {Fragment} from "react";
+import ReactDOM from "react-dom";
+import Dashboard from "./Dashboard";
+
+const App = () => {
+    return (
+        <Fragment>
+            <h1>To Do Items</h1>
+            <Dashboard />
+        </Fragment>
+    )
+};
+
+export default App;
---
+++ src/components/Dashboard.jsx
@@ -0,0 +1,34 @@
+import React, {useState} from 'react';
+import './style.css';
+
+const items = [
+    { name: "do this", isOpen: true},
+    { name: "do that", isOpen: true },
+    { name: "clean shit", isOpen: true},
+    { name: "close the toilet seat", isOpen: true}
+];
+
+const Dashboard = () => {
+     const [todos, setToDos] = useState(items);
+
+    const toggleItem = (item) => {
+         item.isOpen = !item.isOpen
+        console.log(todos)
+        setToDos([...todos])
+    };
+
+
+return (
+        <div>
+            <ul>
+                {todos && todos.map(item =>
+                    <li onClick={() => toggleItem(item)} className={item.isOpen ? 'open' : 'closed'}>
+                        {item.name}
+                    </li>
+                )}
+            </ul>
+        </div>
+
+)};
+
+export default Dashboard;
