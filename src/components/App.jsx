0bee22858fb312341a7272199acbe3d5e763d1b6
Shruti Kapoor <shrkapoor@paypal.com>
2019-10-19 17:26:26 -0700

    wip

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,10 +4,7 @@ import Dashboard from "./Dashboard";
 
 const App = () => {
     return (
-        <Fragment>
-            <h1>To Do Items</h1>
             <Dashboard />
-        </Fragment>
     )
 };
 
